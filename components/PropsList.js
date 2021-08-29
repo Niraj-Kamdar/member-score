c899e4e26230ee135e917ed62f4276f957502071
nicovalencia <hello@nico.io>
2021-08-29 17:23:09 -0600

    Bootstrap frontend React stack. (#1)

---
+++ components/PropsList.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div``
+
+const PropsList = () => {
+  return (
+    <Wrapper>
+      <h2>Props List:</h2>
+      <ul>
+        <li>No props...</li>
+      </ul>
+    </Wrapper>
+  )
+}
+
+export default PropsList
---
+++ readme.md
@@ -0,0 +1,10 @@
+# FWB Props
+
+Member reputation scores for FWB.
+
+# Setup
+
+- Install Node.js (and npm)
+- `npm install next --save -g` install Next.js
+- `npm install` install package dependencies
+- `npm run dev` run development environment
