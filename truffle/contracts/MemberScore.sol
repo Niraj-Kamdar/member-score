81b3c3eba004edcac567fbd05ed32f63750b7cdb
Niraj Kamdar <nirajkamdar27@pm.me>
2021-08-30 04:24:16 +0530

    fix: issues

--- truffle/contracts/MemberScore.sol
+++ truffle/contracts/MemberScore.sol
@@ -5,20 +5,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MemberScore is ERC20, Ownable {
-    uint totalSeasons = 0;
-    uint currentNonce = 0;
+    uint public totalSeasons = 0;
+    uint public currentNonce = 0;
 
     struct Proposal {
         string cid;
         uint season;
         uint score;
     }
+
+    event LogNewScore(string cid, uint nonce, uint season, uint score);
+    event LogNewSeason(uint season);
+    event LogClaimed(uint nonce);
     
-    mapping(address => uint[]) seasonalScore; // user -> seasons[score]
-    mapping(address => uint) totalScore;
-    mapping(uint => address) nonceToUser;
-    mapping(uint => bool) isClaimed;
-    mapping(uint => Proposal) proposals;
+    mapping (address => uint[]) public seasonalScore; // user -> seasons[score]
+    mapping (address => uint) public totalScore;
+    mapping (uint => address) public nonceToUser;
+    mapping (uint => bool) public isClaimed;
+    mapping (uint => Proposal) public proposals;
 
     constructor() ERC20("FWB Reputation Token", "REP") {}
 
@@ -28,18 +32,32 @@ contract MemberScore is ERC20, Ownable {
         currentNonce += 1;
         Proposal newProposal = Proposal(cid, currentSeason, score);
         nonceToUser[_msgSender()] = currentNonce;
+        LogNewScore(cid, currentNonce, currentSeason, score);
     }
 
     function startNewSeason() public onlyOwner {
+        emit LogNewSeason(totalSeasons);
         totalSeasons += 1;
     }
 
     function claim(uint nonce) public {
-        require(!isClaimed[nonce], "REPs hhave been claimed already!");
+        require(!isClaimed[nonce], "REPs have been claimed already!");
         require(nonceToUser[nonce] == _msgSender(), "Only account owner can claim the REP!");
         _mint(_msgSender(), proposals[nonce].score);
         seasonalScore[_msgSender()][proposals[nonce].season] += proposals[nonce].score;
         totalScore[_msgSender()] += proposals[nonce].score;
+        emit LogClaimed(nonce);
     }
 
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        require(false, "Transfer not supported for the REP token!");
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        require(false, "Transfer not supported for the REP token!");
+    }
 }
