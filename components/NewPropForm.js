56b827fba6c96b02cee18f5a7d2c969cccf98137
nicovalencia <hello@nico.io>
2021-08-29 17:03:42 -0600

    Bootstrap frontend React stack.

---
+++ components/NewPropForm.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div``
+
+const NewPropForm = () => {
+  return (
+    <Wrapper>
+      <h2>New Prop:</h2>
+      <form>form...</form>
+    </Wrapper>
+  )
+}
+
+export default NewPropForm
---
+++ components/ui/ConnectButton.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  cursor: pointer;
+`
+
+const ConnectButton = () => (
+  <Wrapper>
+    <h3>Connect</h3>
+  </Wrapper>
+)
+
+export default ConnectButton
